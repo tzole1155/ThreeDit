3e7a4e5b19be1030f6fd6fbd02dd495f5c3d1360
Georgios Albanis <giorgos_al10@hotmail.com>
2023-09-04 11:47:37 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -215,8 +215,8 @@ def main():
     urllib.request.urlretrieve(
         'https://raw.githubusercontent.com/tzole1155/ThreeDit/main/Images/Banner.png',
         os.path.join(static_path,"banner.png"))
-    banner = PIL.Image.open(os.path.join(static_path,"banner.png"))
-    st.image(banner, use_column_width  = True)
+    # banner = PIL.Image.open(os.path.join(static_path,"banner.png"))
+    # st.image(banner, use_column_width  = True)
     st.markdown("<h1 style='text-align: center; color: white;'>Reconstruct your room form a single panorama</h1>", unsafe_allow_html=True)
 
     text_file = open("intro.md", "r")
